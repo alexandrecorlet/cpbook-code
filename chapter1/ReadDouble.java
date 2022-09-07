a3e6cc9c4899f3a4b582d6cf11c467256381047d
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-06 22:15:51 -0300

    Solve Read Double challenge

---
+++ chapter1/ReadDouble.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ReadDouble {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        System.out.printf("'%7.3f'\n", n);
+    }
+}
