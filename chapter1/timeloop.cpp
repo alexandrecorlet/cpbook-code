faa13f584b38ed20ddaf57d03dcc96699e465dc6
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-07 19:24:00 -0300

    Solve Kattis - Stuck In A Time Loop problem

---
+++ chapter1/timeloop.cpp
@@ -0,0 +1,14 @@
+/*
+ * Kattis: Stuck In a Time Loop
+ * author: uncoded
+ */
+
+#include <cstdio>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i)
+        printf("%d Abracadabra\n", i);
+    return 0;
+}
