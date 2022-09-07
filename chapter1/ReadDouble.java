f69299321120c406b280ef1ac0b05906e110b466
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-06 22:17:07 -0300

    Update Read Double

--- chapter1/ReadDouble.java
+++ chapter1/ReadDouble.java
@@ -4,6 +4,6 @@ public class ReadDouble {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double n = sc.nextDouble();
-        System.out.printf("'%7.3f'\n", n);
+        System.out.printf("%7.3f\n", n);
     }
 }
