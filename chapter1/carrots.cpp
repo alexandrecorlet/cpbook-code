15744cb7b9cb5bd00757c6bb6b622e19ca580378
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-07 12:30:16 -0300

    Solve Kattis Carros problem

---
+++ chapter1/carrots.cpp
@@ -0,0 +1,15 @@
+/*
+ * Kattis: Carrots
+ * author: uncoded
+ */
+
+#include <cstdio>
+
+int main() {
+    int n, p;
+    scanf("%d %d", &n, &p);
+    while (n--) 
+        scanf("%*s");
+    printf("%d\n", p);
+    return 0;
+}
