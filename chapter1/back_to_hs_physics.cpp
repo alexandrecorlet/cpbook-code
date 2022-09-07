2d44ab4b050a61c9b620f927053695aff06e0d46
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-07 11:44:30 -0300

    Solve Back to High School Physics

---
+++ chapter1/back_to_hs_physics.cpp
@@ -0,0 +1,13 @@
+/*
+ * UVa 10071 - Back to High School Physics
+ * author: uncoded
+ */
+
+#include <cstdio>
+
+int main() {
+    int v, t;
+    while (scanf("%d %d", &v, &t) != EOF)
+        printf("%d\n", v * t * 2);
+    return 0;
+}
