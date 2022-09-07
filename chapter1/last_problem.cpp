459eafb8c3c879ee4b5f9159c412681f11f7b1eb
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-07 13:42:52 -0300

    Add solution to Kattis The Last Problem

---
+++ chapter1/last_problem.cpp
@@ -0,0 +1,13 @@
+/*
+ * Kattis: The Last Problem
+ * author: uncoded
+ */
+
+#include <cstdio>
+
+int main() {
+    char s[55];
+    scanf("%[A-Z a-z]", s);
+    printf("Thank you, %s, and farewell!\n", s);
+    return 0;
+}
