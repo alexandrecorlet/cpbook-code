a43eb4cbd3d3fe89cad42598e50837ab4f8931fe
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-09 13:41:38 -0300

    Solve Kattis Temperature

---
+++ chapter1/temperature.cpp
@@ -0,0 +1,21 @@
+/*
+ * Kattis: Temperature
+ * author: uncoded
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+
+    scanf("%d %d", &x, &y);
+    if (x == 0 && y == 1)
+        printf("ALL GOOD\n");
+    else if (y == 1)
+        printf("IMPOSSIBLE\n");
+    else
+        printf("%.6lf\n", -x / (y - 1.0));
+
+    return 0;
+}
