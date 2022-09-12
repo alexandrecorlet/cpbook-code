fd1ad2c45f9adc45be43147aed93024a8ec82709
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-12 15:11:54 -0300

    Solve UVa Zapping

---
+++ chapter1/multiple_test_cases_and_selection/zapping.cpp
@@ -0,0 +1,18 @@
+/*
+ * UVa 12468 - Zapping 
+ * author: uncoded
+ */
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b;
+    while (scanf("%d %d", &a, &b), (a != -1 && b != -1)) {
+        int nb_moves = abs(a - b);
+        if (nb_moves > 50) nb_moves = 100 - nb_moves;
+        printf("%d\n", nb_moves);
+    }
+    return 0;
+}
