0c6b14d8c121dd3f6738ffe8b672ad410158a155
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-23 20:50:25 -0300

    Update Ptice solution

--- chapter1/easy/ptice.cpp
+++ chapter1/easy/ptice.cpp
@@ -7,30 +7,24 @@
 #include <algorithm>
 using namespace std;
 
-int getCorrectAnswers(char s[], int n, char guess[], int m) {
-    int j = 0, correct = 0;
-    for (int i = 0; s[i]; ++i) {
-        if (s[i] == guess[j]) 
-            ++correct; 
-        j = (1 + j) % m;
-    }
-    return correct;
-}
-
 int main() {
     int n;
     char s[110];
     scanf("%d %s", &n, s);
     char adrian[] = "ABC", bruno[] = "BABC", goran[] = "CCAABB"; 
-    int a[] = {
-        getCorrectAnswers(s, n, adrian, 3), 
-        getCorrectAnswers(s, n, bruno, 4), 
-        getCorrectAnswers(s, n, goran, 6)
-    };
-    int* maxi = max_element(a, a+3); 
+    int correct[] = {0, 0, 0};
+    for (int i = 0; s[i]; ++i) {
+        if (adrian[i%3] == s[i])
+            ++correct[0];
+        if (bruno[i%4] == s[i])
+            ++correct[1];
+        if (goran[i%6] == s[i])
+            ++correct[2];
+    }
+    int* maxi = max_element(correct, correct+3);
     printf("%d\n", *maxi);
     for (int i = 0; i < 3; ++i)
-        if (a[i] == *maxi)
+        if (correct[i] == *maxi)
             printf("%s\n", i == 0 ? "Adrian" : (i == 1) ? "Bruno" : "Goran");
     return 0;
 }
