543f3de87670257ea1905c01920d9c94f27ddd3b
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-23 20:12:39 -0300

    Solve Mosquito Multiplication

---
+++ chapter1/easy/mosquito.cpp
@@ -0,0 +1,23 @@
+/*
+ * Kattis: Mosquito Multiplication
+ * author: uncoded
+ */
+
+#include <cstdio>
+#include <vector>
+
+int main() {
+    int m, p, l, e, r, s, n;
+    while (scanf("%d %d %d %d %d %d %d", &m, &p, &l, &e, &r, &s, &n) != EOF) {
+        while (n--) {
+            int temp_p = p;
+            int temp_l = l;
+            int temp_m = m;
+            p = temp_l / r;
+            l = temp_m * e;
+            m = temp_p / s;
+        }
+        printf("%d\n", m);
+    }
+    return 0;
+}
